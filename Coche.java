d614dd9f2a5ad5182f47d5d63e1f955d3792d010
lmontielv <luis.montiel@hotmail.com>
2023-03-19 18:00:43 -0400

    tarea_funciones v_2 con comentarios

--- Coche.java
+++ Coche.java
@@ -1,7 +1,9 @@
-
+//Clase Coche que contiene el método incrementar puertas
 public class Coche {
     public int numPuertas = 4;
 
+    /*Método incrementar puertas invocado desde el main 
+    a través de la instancia miCoche*/
     public void incPuertas() {
 
         this.numPuertas++;
--- Tarea_1.java
+++ Tarea_1.java
@@ -3,13 +3,19 @@
 public class Tarea_1 {
     public static void main(String[] args) {
         int resultado = 0;
+        //llamada a la función suma con paso de parámetros por valor
         resultado = suma(10, 20, 30);
         System.out.println(resultado);
 
+        //instancia miCoche para llamar el método incPuertas()
         Coche miCoche = new Coche();
+        
+        /*llamada al método incPuertas() 
+        para usar la función con paso de parámetros por referencia */
         miCoche.incPuertas();
     }
 
+    //Función de suma por paso de parámetros por valor
     public static int suma(int a, int b, int c) {
         return a + b + c;
     }
